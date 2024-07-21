14bf6139f63d08a8df4bdca51b6190cd0670a51f
brysonzkie <jbbustamante07.shs@gmail.com>
2024-07-22 00:54:56 +0800

    fix: change getHumanChoice to getComputerChoice

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //Create a function that will the user choice with "rock, paper, scissors"
 
 
-function getHumanChoice() {
+function getComputerChoice() {
     var choices = ['rock', 'paper','scissors'];
     var randomIndex = Math.floor(Math.random() * choices.length);
     return choices[randomIndex];
