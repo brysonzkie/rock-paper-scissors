4537dca9e756f5e61a6160d7182d423626666c88
brysonzkie <jbbustamante07.shs@gmail.com>
2024-07-24 23:25:57 +0800

    enhance: added comments explaining what the function does.

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
-//Create a global scope variable for their scores
-
-let humanScore = 0;
-let computerScore = 0;
+//create a game called rock-paper-scissors
+//the game will be played users vs computers
 
 
+//Create a function getComputerChoice that will randomly returns "rock", "paper" and "scissors"
 
-//Create a function that will let the computer randomly choose "rock, paper, scissors"
 function getComputerChoice() {
-    var choices = ['rock', 'paper','scissors'];
-    var randomIndex = Math.floor(Math.random() * choices.length);
-    return choices[randomIndex];
-   
+    const computerChoices = ['rock', 'paper', 'scissors'];
+    const randomize =  Math.floor(Math.random() * computerChoices.length );
+    return computerChoices[randomize];
 }
 
-//Create a function that will let the users choice "rock, paper, scissors"
+//Create a function getHumanChoice that will let the user choice within the three choices
+
 function getHumanChoice() {
     let userChoice = prompt("Please choice of these three: 'rock, paper, scissors '")
 
@@ -27,8 +25,19 @@ function getHumanChoice() {
 }
 
 
-//Create a function that will determine the winner based on the user and computer's choices
+//Create a two new variables named humanScore and computerScore in the global scope
+//initialize those variables to 0
 
+let humanScore = 0;
+let computerScore = 0;
+
+
+//Create a function that takes the human and computer player choices as arguments,
+//plays a single round, increments the round winner’s score and logs a winner announcement.
+
+
+//Create a function called playRound that has two parameters: humanChoice and computerChoice
+//also this will serve as their first round 
 function playRound(humanChoice, computerChoice) {
     if (humanChoice === computerChoice) {
         return 'It\'s a tie!';
@@ -36,35 +45,39 @@ function playRound(humanChoice, computerChoice) {
     if (humanChoice.toLowerCase() === 'rock') {
         if (computerChoice === 'scissors'){
             humanScore++;
-            return 'You win!';
+            return 'You win! Rock beats Scissors';
         } else if (computerChoice === 'paper') {
             computerScore++;
-            return 'You lost, Computer wins';
+            return 'You lost, Computer wins! Papers beats Rocks';
         }
     }
     if (humanChoice.toLowerCase() === 'paper') {
         if (computerChoice === 'rock'){
             humanScore++;
-            return 'You win!';
+            return 'You win! Paper beats Rocks';
         } else if (computerChoice ==='scissors') {
             computerScore++;
-            return 'You lost, Computer wins';
+            return 'You lost, Computer wins! Scissors beats Papers';
         }
     }
     if (humanChoice.toLowerCase() ==='scissors') {
         if (computerChoice === 'paper'){
             humanScore++;
-            return 'You win!';
+            return 'You win! Scissors beats Paper';
         } else if (computerChoice === 'rock') {
             computerScore++;
-            return 'You lost, Computer wins';
+            return 'You lost, Computer wins! Rocks beats Scissors';
         }
     }
     return 'Invalid input. Please choose rock, paper, or scissors.';
 
 }
 
-//Create a function that will play the game for 5 rounds
+
+
+//Write the logic to play the entire game that means 5 rounds 
+//Create a function playGame that will call the playRound function to play 5 rounds 
+//It should keep track of the scores and declares a winner at the end   
 
 function playGame() {
     for (let i = 0; i < 5; i++) {
